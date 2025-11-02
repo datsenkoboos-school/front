614c2a8f0dc19d43326dd27c183fe4d01a33cab5
Arthur Datsenko-Boos <adacenkoboos@gmail.com>
2025-11-02 12:04:39 +0700

    format code

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,27 @@
-import js from '@eslint/js';
-import prettier from 'eslint-config-prettier';
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
 
 export default [
   {
-    ignores: ['node_modules/**', 'dist/**', 'build/**', 'coverage/**', 'public/**'],
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "build/**",
+      "coverage/**",
+      "public/**",
+    ],
   },
   js.configs.recommended,
   prettier,
   {
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: 'module',
+      sourceType: "module",
       globals: {
-        window: 'readonly',
-        document: 'readonly',
-        console: 'readonly',
-        process: 'readonly',
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        process: "readonly",
       },
     },
     rules: {},
